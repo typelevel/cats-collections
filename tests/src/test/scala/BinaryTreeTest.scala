3772f73cf9185183f52d5c2ad693011b9579be27
Nicolas A Perez <anicolaspp@gmail.com>
2016-01-31 23:38:46 -0500

    Refactoring Tests

--- tests/src/test/scala/BinaryTreeTest.scala
+++ tests/src/test/scala/BinaryTreeTest.scala
@@ -20,13 +20,13 @@ class BinaryTreeTestAdd extends FlatSpec with Matchers {
     }
   }
 
-  "a empty tree" should "be a leaft" in {
+  "Binary Tree" should "be a leaft when empty" in {
     val emptyTree = BinaryTree.empty[Int]
 
     emptyTree should be(BTNil())
   }
 
-  "add to empty tree" should "return a branch with one item" in {
+  it  should "return a branch with one item when add to empty tree" in {
 
     val empty = BinaryTree.empty[Int]
 
@@ -35,7 +35,7 @@ class BinaryTreeTestAdd extends FlatSpec with Matchers {
     branch.value should be(Some(5))
   }
 
-  "add to brach to the left" should "insert value into left branch" in {
+  it should "insert value into left branch when add to brach to the left" in {
 
     val branch = BinaryTree(5)
 
@@ -45,7 +45,7 @@ class BinaryTreeTestAdd extends FlatSpec with Matchers {
     result.left.value should be(Some(3))
   }
 
-  "add to branch to the right" should "insert value into right branch" in {
+  it should "insert value into right branch when add to branch to the right" in {
 
     val branch = BinaryTree(5)
 
@@ -55,7 +55,7 @@ class BinaryTreeTestAdd extends FlatSpec with Matchers {
     result.right.value should be(Some(7))
   }
 
-  "add" should "balance the tree after item added" in {
+  it should "balance the tree after item added" in {
     val tree = BinaryTree(3) + 2 + 1
 
     tree.value should be(Some(2))
@@ -85,7 +85,7 @@ class BinaryTreeTestJoins extends FlatSpec with Matchers {
     }
   }
 
-  "a tree" should "be the same after joined with an empty tree" in {
+  "Binary Tree" should "be the same after joined with an empty tree" in {
     val tree = BinaryTree(5) + 3 + 2 + 7 + 9
 
     val empty = BinaryTree.empty[Int]
@@ -114,7 +114,7 @@ class BinaryTreeTestWalk extends FlatSpec with Matchers {
     }
   }
 
-  "toLst" should "return sorted seq" in {
+   "Binary Tree"  should "return sorted seq when toLst" in {
     val tree = BinaryTree(5).add(2).add(1).add(3)
 
     val sorted = tree.toLst
@@ -122,7 +122,7 @@ class BinaryTreeTestWalk extends FlatSpec with Matchers {
     ScalaListConverter.toScalaList(sorted) should contain inOrder(1, 2, 3, 5)
   }
 
-  "walk in in-order" should "return sorted seq" in {
+  it  should "return sorted seq when walk in-order" in {
     val tree = BinaryTree(5).add(2).add(1).add(3)
 
     val sorted = tree.inorder
@@ -130,7 +130,7 @@ class BinaryTreeTestWalk extends FlatSpec with Matchers {
     sorted should be (tree.toLst)
   }
 
-  "walk in pre-order" should "return node-left-right for each node" in {
+  it  should "return node-left-right for each node when walk in pre-order" in {
     val tree = BinaryTree(5).add(2).add(1).add(3)
 
     val preorder = tree.preorder
@@ -138,7 +138,7 @@ class BinaryTreeTestWalk extends FlatSpec with Matchers {
     ScalaListConverter.toScalaList(preorder) should contain inOrder(2, 1, 5, 3)
   }
 
-  "walk in pos-order" should "return left-right-node for each node" in {
+  it  should "return left-right-node for each node when walk in pos-order" in {
     val tree = BinaryTree(5).add(2).add(1).add(3) + 6
 
     val preorder = tree.posorder
@@ -146,19 +146,19 @@ class BinaryTreeTestWalk extends FlatSpec with Matchers {
     ScalaListConverter.toScalaList(preorder) should contain inOrder(1, 3, 6, 5, 2)
   }
 
-  "min" should "return most left item" in {
+  it  should "return most left item when ask for min" in {
     val tree = BinaryTree(5).add(6).add(7).add(2).add(3).add(1)
 
     tree.min() should be(Some(1))
   }
 
-  "max" should "return most right item" in {
+  it should "return most right item when ask for max" in {
     val tree = BinaryTree(5).add(6).add(7).add(2).add(3).add(1)
 
     tree.max() should be(Some(7))
   }
 
-  "min and max" should "return some" in {
+  it should "return none when ask for min / max in empty tree" in {
     val empty = BinaryTree.empty[Int]
 
     empty.min() should be(None())
@@ -175,13 +175,13 @@ class BinaryTreeTestRemove extends FlatSpec with Matchers {
     }
   }
 
-  "remove" should "return DBNil when empty" in {
+  "Binary Tree" should "return DBNil when remove from empty tree" in {
     val empty = BinaryTree.empty[Int]
 
     empty should be(empty.remove(5))
   }
 
-  "remove leaft" should "keep same tree structure - leaft" in {
+  it should "keep same tree structure - leaft when remove leaft" in {
     val tree = BinaryTree(5).add(2).add(1).add(3)
 
     val result = tree.remove(1)
@@ -191,7 +191,7 @@ class BinaryTreeTestRemove extends FlatSpec with Matchers {
     ScalaListConverter.toScalaList(sorted) should contain inOrder(2, 3, 5)
   }
 
-  "remove to the left" should "point parent to child of the one removed (left)" in {
+  it should "point parent to child of the one removed (left) when remove to the left" in {
     val tree = BinaryTree(5).add(2).add(1).add(3)
 
     val result = tree.remove(2)
@@ -201,7 +201,7 @@ class BinaryTreeTestRemove extends FlatSpec with Matchers {
     ScalaListConverter.toScalaList(sorted) should contain inOrder(1, 3, 5)
   }
 
-  "remove to right" should "point parent to child of of the one removed (right)" in {
+  it should "point parent to child of of the one removed (right) when remove to right" in {
     val tree = BinaryTree(5) + 7 + 8 + 2 + 1
 
     val result = tree + 3
@@ -220,7 +220,7 @@ class BinaryTreeTest extends FlatSpec with Matchers{
     }
   }
 
-  "Binary" should "return false when asking for non-existence item" in {
+  "Binary Tree" should "return false when asking for non-existence item" in {
     val tree = BinaryTree(5) + 4 + 1
 
     tree.contains(2) should be(false)
