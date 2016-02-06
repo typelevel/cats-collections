6154eb613ef3836f7405491943aac10daaafe502
Stew O'Connor <stew@vireo.org>
2016-02-06 14:12:58 -0800

    add more operators to order

--- core/src/main/scala/Order.scala
+++ core/src/main/scala/Order.scala
@@ -28,11 +28,45 @@ trait Order[A] { self =>
     override def apply(l: B, r: B) = self(f(l), f(r))
   }
 
+  /**
+   * Test if the two given values are equal
+   */
   def eq(l: A, r: A): Boolean = apply(l, r) == EQ
 
+  /**
+   * Test if the left is greater than the right
+   */
+  def gt(l: A, r: A): Boolean = apply(l, r) == GT
+
+  /**
+   * Test if the left is greater than or equal to the right
+   */
+  def ge(l: A, r: A): Boolean = apply(l, r) != LT
+
+  /**
+   * Test if the left is less than the right
+   */
+  def lt(l: A, r: A): Boolean = apply(l, r) == LT
+
+  /**
+   * Test if the left is less than or equal to the right
+   */
+  def le(l: A, r: A): Boolean = apply(l, r) != GT
+
+  /**
+   * Returns the lesser of the two given values.
+   */
   def min(l: A, r: A): A = compare(l,r) match {
     case LT => l
     case _ => r
+  }
+
+  /**
+   * Returns the greater of the two given values.
+   */
+  def max(l: A, r: A): A = compare(l,r) match {
+    case LT => r
+    case _ => l
   }
 }
 
@@ -42,5 +76,15 @@ object Order {
   case object EQ extends Ordering
   case object GT extends Ordering
 
+  /**
+   * summon an Order from implicit scope
+   */
   def apply[A](implicit A: Order[A]): Order[A] = A
+
+  /**
+   * A convenience method for defining an ordering
+   */
+  def instance[A](f: (A,A) => Ordering): Order[A] = new Order[A] {
+    def apply(l: A, r: A): Ordering = f(l,r)
+  }
 }
