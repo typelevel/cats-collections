7a5176292b545f971d061deb3aa5204ce6c5a7a5
Nicolas A Perez <anicolaspp@gmail.com>
2016-02-08 14:54:20 -0500

    difference with another range (need this to optimize add range in Diet)

--- tests/src/test/scala/dogs/ARangeTest.scala
+++ tests/src/test/scala/dogs/ARangeTest.scala
@@ -34,7 +34,13 @@ class ARangeTest extends FlatSpec with Matchers {
     range.contains(101) should be (false)
   }
 
-  it should "apply function to each elemet" in {
+  it should "contain sub range" in {
+    val range = ARange(1, 10)
+
+    range.contains(ARange(2,9)) should be (true)
+  }
+
+  it should "apply function to each element" in {
     var s = 0
 
     val range = ARange(1, 100)
@@ -56,5 +62,29 @@ class ARangeTest extends FlatSpec with Matchers {
     val range = ARange(1, 100)
 
     range.foldLeft[Int](0, (a,b) => a + b) should be (1.to(100).sum)
+  }
+
+  it should "be able to diff (-)" in {
+    val range = ARange(1, 10)
+
+    val (l, r) = range - ARange(2,9)
+
+    l.generate().toScalaList should be(scala.List(1))
+    r.generate().toScalaList should be(scala.List(10))
+
+    val (l1, r1) = range - range
+
+    l1 should be (ARange.empty())
+    r1 should be (ARange.empty())
+
+    val (l2, r2) = range - ARange(-1, 5)
+
+    l2 should be (ARange.empty())
+    r2.generate().toScalaList should contain inOrderOnly (6, 7, 8, 9, 10)
+
+    val (l3, r3) = range - ARange(3, 12)
+
+    l3.generate().toScalaList should contain inOrderOnly(1, 2)
+    r3 should be (ARange.empty())
   }
 }
