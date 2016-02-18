3f161559105eb8648518d047e43ea89300b8520e
Nicolas A Perez <anicolaspp@gmail.com>
2016-02-18 13:57:24 -0500

    ref

--- bench/src/main/scala/DietDataGen.scala
+++ bench/src/main/scala/DietDataGen.scala
@@ -1,16 +1,15 @@
+/**
+  * Created by anicolaspp on 2/18/16.
+  */
+
 package dogs
 package bench
 
 import dogs.Predef._
 
-/**
-  * Created by anicolaspp on 2/18/16.
-  */
 object DietDataGen {
 
   def getWorstCaseData: scala.IndexedSeq[Int] = {
-    var x = 0
-
     for (x <- scala.Range(1, 1000)
       if (x % 2 == 0)
     ) yield x
