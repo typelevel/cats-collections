748591b0a1c7a5a4364e73ad50be50ebe7e778fc
Nicolas A Perez <anicolaspp@gmail.com>
2016-02-25 20:18:27 -0500

    more changes in Diet Bench to use BigNumberLists trait

--- bench/src/main/scala/DietRandomizeBench.scala
+++ bench/src/main/scala/DietRandomizeBench.scala
@@ -14,9 +14,9 @@ import scalaz.Diev
   * In reality, no one uses the best and worst scenario, so this is a complete randomized benchmark
   */
 @State(Scope.Benchmark)
-class DietRandomizeBench {
+class DietRandomizeBench extends BigNumberLists{
 
-  import dogs.Predef._
+//  import dogs.Predef._
 
   implicit val scalazEnumInt = scalaz.std.anyVal.intInstance
 
@@ -24,38 +24,27 @@ class DietRandomizeBench {
   def dogsDietAddRandom: Unit = {
     var diet = Diet.empty[Int]
 
-    (1 to 10000).foreach {_ =>
-      diet = diet + Random.nextInt()
-    }
+    Random.shuffle(dogs.toScalaList).foreach{ i => diet = diet + i }
   }
 
   @Benchmark
   def scalazDievAddRandom: Unit = {
     var diev = Diev.empty[Int]
 
-    (1 to 10000).foreach {_ =>
-      diev = diev + Random.nextInt()
-    }
+    Random.shuffle(scalazlst.toList).foreach { i => diev = diev + i}
   }
 
   @Benchmark
   def dogsDietAddRangeRandom: Unit = {
     var diet = Diet.empty[Int]
 
-    (1 to 10000).foreach {_ =>
-      val i = Random.nextInt()
-      diet = diet + dogs.Range(i, i + 10)
-    }
+    Random.shuffle(dogs.toScalaList).foreach { i => diet = diet + Range(i, i + 10)}
   }
 
   @Benchmark
   def scalazDievAddRangeRandom: Unit = {
     var diev = Diev.empty[Int]
 
-    (1 to 10000).foreach {_ =>
-      val i = Random.nextInt()
-
-      diev = diev + (i, i + 10)
-    }
+    Random.shuffle(scalazlst.toList).foreach { i => diev = diev + (i, i + 10)}
   }
 }
