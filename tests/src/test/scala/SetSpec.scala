7b14e6565bf8c075de81d95351e973798d24952d
Stew O'Connor <stew@vireo.org>
2016-03-20 21:48:33 -0700

    convert SetSpec to DogsSuite

--- tests/src/test/scala/SetSpec.scala
+++ tests/src/test/scala/SetSpec.scala
@@ -1,4 +1,5 @@
 package dogs
+package tests
 
 import dogs.Predef._
 import dogs.tests.arbitrary._
@@ -10,17 +11,17 @@ import scala.Array
 import scala.Predef.intArrayOps
 import algebra.std.int._
 
-class SetSpec extends Properties("Set") with ArbitraryList {
+class SetSpec extends DogsSuite with ArbitraryList {
   import scala.collection.immutable.{Set => SSet, Map => MMap}
 
-  property("set is always sorted") = forAll { (xs: List[Int]) =>
+  test("set is always sorted")(forAll { (xs: List[Int]) =>
     val tree = xs.foldLeft(Set.empty[Int])(_ + _)
 
     val ours: scala.List[Int] = tree.toList.toScalaList
     val theirs: scala.List[Int] = xs.toScalaList.to[SSet].to[Array].sorted.to[scala.List]
 
-    ours == theirs
-  }
+    ours should be (theirs)
+  })
 
   import Set._
   def balanced[A](t: Set[A]): Boolean = t match {
@@ -30,70 +31,72 @@ class SetSpec extends Properties("Set") with ArbitraryList {
 
   }
 
-  property("set is always balanced") = forAll { (xs: List[Int]) =>
+  test("set is always balanced")(forAll { (xs: List[Int]) =>
     val tree = Set(xs.toScalaList: _*)
-    balanced(tree)
-  }
+    balanced(tree) should be(true)
+  })
 
-  property("set can delete") = forAll{ (xs: MMap[Int,Boolean]) =>
+  test("set can delete")(forAll{ (xs: MMap[Int,Boolean]) =>
     val tree = Set(xs.keySet.toSeq: _*)
     val filtered = xs.foldLeft(tree)((t,i) => if(i._2) t else t.remove(i._1))
 
     val ours: SSet[Int] = filtered.toList.toScalaList.to[SSet]
     val theirs: SSet[Int] = xs.collect{ case (i, true) => i }.to[SSet]
 
-    (ours == theirs) && balanced(filtered)
-  }
+    ours should be (theirs)
+    balanced(filtered) should be(true)
+  })
 
-  property("contains works") = forAll{ (xs: MMap[Int,Boolean]) =>
+  test("contains works")(forAll{ (xs: MMap[Int,Boolean]) =>
     val tree = xs.foldLeft[Set[Int]](Set.empty)((t,i) =>
       if(i._2) t + i._1 else t
     )
 
     xs.map{
       case (k,v) => tree.contains(k) == v
-    }.foldLeft(true)(_ && _)
-  }
+    }.foldLeft(true)(_ && _) should be(true)
+  })
 
-  property("find works") = forAll{ (xs: MMap[Int,Boolean]) =>
+  test("find works")(forAll{ (xs: MMap[Int,Boolean]) =>
     val tree = xs.foldLeft[Set[Int]](Set.empty)((t,i) =>
       if(i._2) t + i._1 else t
     )
 
     xs.map{
       case (k,v) => tree.find(_ == k).isDefined == v
-    }.foldLeft(true)(_ && _)
-  }
+    }.foldLeft(true)(_ && _) should be(true)
+  })
 
-  property("intersect is correct") = forAll{ (xs: SSet[Int], ys: SSet[Int]) =>
+  test("intersect is correct")(forAll{ (xs: SSet[Int], ys: SSet[Int]) =>
     val xt = Set(xs.toSeq: _*)
     val yt = Set(ys.toSeq: _*)
 
-    (xt intersect yt).toScalaSet == (xs intersect ys) && (xt & yt).toScalaSet == (xs intersect ys)
+    (xt intersect yt).toScalaSet should be (xs intersect ys)
+    (xt & yt).toScalaSet should be(xs intersect ys)
+  })
 
-  }
-
-  property("union is correct") = forAll{ (xs: SSet[Int], ys: SSet[Int]) =>
+  test("union is correct")(forAll{ (xs: SSet[Int], ys: SSet[Int]) =>
     val xt = Set(xs.toSeq: _*)
     val yt = Set(ys.toSeq: _*)
 
-    (xt union yt).toScalaSet == (xs union ys) && (xt | yt).toScalaSet == (xs union ys) 
+    (xt union yt).toScalaSet should be(xs union ys)
+    (xt | yt).toScalaSet should be(xs union ys)
+  })
 
-  }
-
-  property("we can take the difference of sets") = forAll{ (xs: SSet[Int], ys: SSet[Int]) =>
+  test("we can take the difference of sets")(forAll{ (xs: SSet[Int], ys: SSet[Int]) =>
     val xt = Set(xs.toSeq: _*)
     val yt = Set(ys.toSeq: _*)
 
-    (xt diff yt).toScalaSet == (xs diff ys) && (xt - yt).toScalaSet == (xs diff ys)
-  }
+    (xt diff yt).toScalaSet should be(xs diff ys)
+    (xt - yt).toScalaSet should be(xs diff ys)
+  })
 
-  property("map works") = forAll{ (xs: SSet[Int]) =>
+  test("map works") (forAll{ (xs: SSet[Int]) =>
     val f: Int => Int = _ + 1
 
     val xt = Set(xs.toSeq: _*)
 
-    (xt map f).toScalaSet == (xs map f)
-  }
+    (xt map f).toScalaSet should be(xs map f)
+  })
 }
 
