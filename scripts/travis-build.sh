3e48a0013295ac33235c951c6dcd758656883b49
Stew O'Connor <stew@vireo.org>
2016-03-30 22:04:37 -0700

    publish from travis

--- scripts/travis-build.sh
+++ scripts/travis-build.sh
@@ -3,6 +3,14 @@ sbt_cmd="sbt ++$TRAVIS_SCALA_VERSION"
 
 export publish_cmd="publishLocal"
 
+if [[ $TRAVIS_PULL_REQUEST == "false" && $TRAVIS_BRANCH == "master" && $(cat version.sbt) =~ "-SNAPSHOT" ]]; then
+  export publish_cmd="publish gitSnapshots publish"
+  # temporarily disable to stabilize travis
+  #if [[ $TRAVIS_SCALA_VERSION = "2.11.8" ]]; then
+  #  export publish_cmd="$publish_cmd ghpagesPushSite"
+  #fi
+fi
+
 coverage="$sbt_cmd coverage validateJVM coverageReport && bash <(curl -s https://codecov.io/bash)"
 scala_js="$sbt_cmd coreJS/compile" # && $sbt_cmd testsJS/test"
 scala_jvm="$sbt_cmd validateJVM"
