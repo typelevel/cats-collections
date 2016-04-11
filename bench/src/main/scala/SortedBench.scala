c4d1d91df5cbf0a35333c48cf272adad922594e6
Nicolas A Perez <anicolaspp@gmail.com>
2016-04-10 20:06:43 -0400

    some Sorted Benchmarks

---
+++ bench/src/main/scala/SortedBench.scala
@@ -0,0 +1,36 @@
+/**
+  * Created by anicolaspp on 4/10/16.
+  */
+
+import dogs.{Sorted, El}
+import org.openjdk.jmh.annotations.{Benchmark, Setup, Scope, State}
+import scala.util.Random
+
+
+@State(Scope.Benchmark)
+class SortedBench {
+
+  var l = El[Int]
+
+  @Setup
+  def setup: Unit = {
+    var i = 0
+    while (i < 100000) {
+      val s = Random.nextInt()
+
+      l = l.::(s)
+
+      i = i + 1
+    }
+  }
+
+  @Benchmark
+  def quicksort: Unit = {
+    Sorted.quickSort(l)
+  }
+
+  @Benchmark
+  def heapsort: Unit = {
+    Sorted.heapSort(l)
+  }
+}
