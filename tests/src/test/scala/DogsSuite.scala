70f3a731d7a37285b7736b6f7ba4c8e6aca7a2b8
Alistair Johnson <alistair.johnson@johnsonusm.com>
2016-04-19 17:43:43 +0200

    Fix travis settings2

--- tests/src/test/scala/DogsSuite.scala
+++ tests/src/test/scala/DogsSuite.scala
@@ -20,7 +20,7 @@ trait TestSettings extends Configuration with Matchers {
   lazy val checkConfiguration: PropertyCheckConfiguration =
     if (Platform.isJvm)
       if(scala.sys.env.get("TRAVIS").isDefined)
-        PropertyCheckConfiguration(minSuccessful = 10, maxDiscardedFactor = 5F, minSize = 0, sizeRange = 10)
+        PropertyCheckConfiguration(minSuccessful = 3, maxDiscardedFactor = 5F, minSize = 0, sizeRange = 5)
       else
         PropertyCheckConfiguration(minSuccessful = 100, maxDiscardedFactor = 5F, minSize = 0, sizeRange = 100)
     else
@@ -29,7 +29,7 @@ trait TestSettings extends Configuration with Matchers {
   lazy val slowCheckConfiguration: PropertyCheckConfiguration =
     if (Platform.isJvm)
       if(scala.sys.env.get("TRAVIS").isDefined)
-        PropertyCheckConfiguration(minSuccessful = 1, maxDiscardedFactor = 5F, minSize = 0, sizeRange = 10)
+        PropertyCheckConfiguration(minSuccessful = 1, maxDiscardedFactor = 5F, minSize = 0, sizeRange = 3)
       else
         PropertyCheckConfiguration(minSuccessful = 1, maxDiscardedFactor = 5F, minSize = 0, sizeRange = 10)
     else
