4cbd7e50f97bce34bd01088444a418dc65c114ee
Stew O'Connor <stew@vireo.org>
2016-08-20 08:09:04 -0700

    only need validate once?"

--- scripts/travis-build.sh
+++ scripts/travis-build.sh
@@ -9,7 +9,7 @@ fi
 
 coverage="$sbt_cmd coverage validateJVM coverageReport && bash <(curl -s https://codecov.io/bash)"
 scala_js="$sbt_cmd coreJS/compile" # && $sbt_cmd testsJS/test"
-scala_jvm="$sbt_cmd validateJVM"
+scala_jvm="$sbt_cmd coreJVM/compile
 
 run_cmd="$coverage && $scala_js && $scala_jvm $publish_cmd"
 eval $run_cmd
