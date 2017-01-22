9ca723bcbefb441e05f6243a0558174b3d9cdbd2
Alexander Konovalov <alex.knvl@gmail.com>
2017-01-22 17:19:18 -0500

    ISet made contravariant.

--- core/src/main/scala/dogs/ISet.scala
+++ core/src/main/scala/dogs/ISet.scala
@@ -8,69 +8,69 @@ import cats._
  * elements as a extensional set does, it is defined by a predicate
  * which is a test for membership.
  */
-abstract class ISet[A] extends scala.Function1[A, Boolean] { self =>
+abstract class ISet[-A] extends scala.Function1[A, Boolean] { self =>
   /**
    * returns a set which is the union of this set and another
    */
-  def union(other: ISet[A]): ISet[A] = ISet(a => self(a) || other(a))
+  def union[B <: A](other: ISet[B]): ISet[B] = ISet(a => apply(a) || other(a))
 
   /**
    * returns a set which is the union of this set and another
    */
-  def |(other: ISet[A]): ISet[A] = ISet(a => self(a) || other(a))
+  def |[B <: A](other: ISet[B]): ISet[B] = ISet(a => apply(a) || other(a))
 
   /**
    * returns a set which is the intersection of this set and another
    */
-  def intersection(other: ISet[A]): ISet[A] = ISet(a => self(a) && other(a))
+  def intersection[B <: A](other: ISet[B]): ISet[B] = ISet(a => apply(a) && other(a))
 
   /**
    * returns a set which is the intersection of this set and another
    */
-  def &(other: ISet[A]): ISet[A] = ISet(a => self(a) && other(a))
+  def &[B <: A](other: ISet[B]): ISet[B] = ISet(a => apply(a) && other(a))
 
   /**
    * Returns true if the value is a emmeber of the set.
    */
-  def contains(a: A): Boolean = self(a)
+  def contains(a: A): Boolean = apply(a)
 
   /**
    * Returns the set which is the the differece of another set removed from this set
    */
-  def diff(remove: ISet[A]): ISet[A] = ISet(a => self(a) && !remove(a))
+  def diff[B <: A](remove: ISet[B]): ISet[B] = ISet(a => apply(a) && !remove(a))
 
   /**
    * Returns the set which is the the differece of another set removed from this set
    */
-  def -(remove: ISet[A]): ISet[A] = ISet(a => self(a) && !remove(a))
+  def -[B <: A](remove: ISet[B]): ISet[B] = ISet(a => apply(a) && !remove(a))
 
   /**
    * Return the set of all As which are not in this set.
    */
-  def negate: ISet[A] = ISet(a => !self(a))
+  def negate: ISet[A] = ISet(a => !apply(a))
 
   /**
    * Return the set of all As which are not in this set.
    */
-  def unary_!(): ISet[A] = ISet(a => !self(a))
+  def unary_!(): ISet[A] = negate
 }
 
 object ISet extends ISetInstances {
-  def apply[A](f: A => Boolean) = new ISet[A] {
+  def apply[A](f: A => Boolean): ISet[A] = new ISet[A] {
     def apply(a: A) = f(a)
   }
 
-  def empty[A] = apply((a: A) => false)
+  def empty: ISet[Any] = apply(_ => false)
 }
 
 trait ISetInstances {
   implicit def isetMonoid[A]: Monoid[ISet[A]] = new Monoid[ISet[A]] {
-    override def empty: ISet[A] = ISet.empty[A]
+    override def empty: ISet[A] = ISet.empty
     override def combine(l: ISet[A], r: ISet[A]): ISet[A] = l union r
   }
 
   implicit val isetInstance: MonoidK[ISet] = new MonoidK[ISet] {
-    override def empty[A]: ISet[A] = ISet.empty[A]
+    override def empty[A]: ISet[A] = ISet.empty
     override def combineK[A](l: ISet[A], r: ISet[A]): ISet[A] = l union r
   }
 }
