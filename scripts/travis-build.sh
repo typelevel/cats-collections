05d91cf97d265af89fff95d37b695a06e922996a
Mike (stew) O'Connor <stew@vireo.org>
2017-07-24 18:09:28 -0700

    try to fix travis build script

--- scripts/travis-build.sh
+++ scripts/travis-build.sh
@@ -7,9 +7,8 @@ if [[ $TRAVIS_PULL_REQUEST == "false" && $TRAVIS_BRANCH == "master" ]] && grep -
   export publish_cmd="publish gitSnapshots publish"
 fi
 
-coverage="(export SCOVERAGEON=true; $sbt_cmd coverage testsJVM/test coverageReport && bash <(curl -s https://codecov.io/bash) )"
-scala_js="$sbt_cmd clean coreJS/compile" # && $sbt_cmd testsJS/test"
-scala_jvm="$sbt_cmd validateJVM"
+coverage="(export SCOVERAGEON=true; $sbt_cmd coverage tests/test coverageReport && bash <(curl -s https://codecov.io/bash) )"
+scala_jvm="$sbt_cmd validate"
 
-run_cmd="$coverage && $scala_js && $scala_jvm $publish_cmd"
+run_cmd="$coverage && $scala_jvm $publish_cmd"
 eval $run_cmd
