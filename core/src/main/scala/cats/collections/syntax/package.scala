25a6e4660fedf0338d3f6fabf06c7cb11eebf440
Lars Hupel <lars.hupel@mytum.de>
2018-09-24 19:27:01 +0200

    deprecate birds

--- core/src/main/scala/cats/collections/syntax/package.scala
+++ core/src/main/scala/cats/collections/syntax/package.scala
@@ -7,10 +7,11 @@ package object syntax {
       RangeSyntax     with
       StreamingSyntax
 
-  object birds     extends BedazzleBirds
   object foldable  extends FoldableSyntax
   object range     extends RangeSyntax
 
   @deprecated("Streaming is obsolete. Use either fs2, Monix, or iteratees.", "cats-collections 0.7.0")
   object streaming extends StreamingSyntax
+  @deprecated("Bird operators are provided by mouse", "cats-collections 0.7.0")
+  object birds     extends BedazzleBirds
 }
