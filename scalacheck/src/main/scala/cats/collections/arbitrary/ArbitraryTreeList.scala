ff45112197c8cb61b9a3a35c108c9af1308c75f4
P. Oscar Boykin <johnynek@users.noreply.github.com>
2019-01-06 05:02:03 -1000

    Add TreeList, an Okasaki immutable list with fast indexing (#152)

---
+++ scalacheck/src/main/scala/cats/collections/arbitrary/ArbitraryTreeList.scala
@@ -0,0 +1,14 @@
+package cats.collections
+package arbitrary
+
+import org.scalacheck.{Cogen, Arbitrary}
+
+trait ArbitraryTreeList {
+  implicit def arbitraryTreeList[A: Arbitrary]: Arbitrary[TreeList[A]] =
+    Arbitrary(Arbitrary.arbitrary[List[A]].map(TreeList.fromListReverse(_)))
+
+  implicit def cogenTreeList[A: Cogen]: Cogen[TreeList[A]] =
+    Cogen[List[A]].contramap(_.toList)
+}
+
+object ArbitraryTreeList extends ArbitraryTreeList
