e1b06ccedc87e4373296623cb4743bfe4951e015
Stephen Lazaro <stephen.lazaro@gmail.com>
2020-02-14 01:26:13 -0800

    Clean Up Predicate A Bit (#269)

--- tests/src/test/scala/cats/collections/PredicateSpec.scala
+++ tests/src/test/scala/cats/collections/PredicateSpec.scala
@@ -1,13 +1,26 @@
 package cats.collections
 package tests
 
-import cats.laws.discipline.SerializableTests
+import cats.collections.arbitrary.predicate._
+import cats.laws.discipline.{ContravariantMonoidalTests, SerializableTests}
 import cats._
+import cats.instances.int._
 import cats.tests.CatsSuite
 
 class PredicateSpec extends CatsSuite {
-
+  checkAll("Monoid[Predicate[Int]]", SerializableTests.serializable(Monoid[Predicate[Int]]))
   checkAll("MonoidK[Predicate]", SerializableTests.serializable(MonoidK[Predicate]))
+  checkAll("Serializable[ContravariantMonoidal[Predicate]]", SerializableTests.serializable(ContravariantMonoidal[Predicate]))
+
+  {
+    implicit val eqForPredicateInt: Eq[Predicate[Int]] = new Eq[Predicate[Int]] {
+      override def eqv(x: Predicate[Int], y: Predicate[Int]): Boolean = x(0) === y(0)
+    }
+    implicit val eqForPredicateTripleInt: Eq[Predicate[(Int, Int, Int)]] = new Eq[Predicate[(Int, Int, Int)]] {
+      override def eqv(x: Predicate[(Int, Int, Int)], y: Predicate[(Int, Int, Int)]): Boolean = x((0, 0, 0)) === y((0, 0, 0))
+    }
+    checkAll("ContravariantMonoidal[Predicate]", ContravariantMonoidalTests[Predicate].contravariantMonoidal[Int, Int, Int])
+  }
 
   test("intersection works")(
     forAll { (as: List[Int], bs: List[Int]) =>
