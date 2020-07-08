8ca8a622a750b963a7224ed6960410372fc40bef
Anthony Cerruti <soren@disroot.org>
2020-07-07 20:03:32 -0700

    Initial apply, empty, get, + for HashMap

--- core/src/main/scala/cats/collections/Map.scala
+++ core/src/main/scala/cats/collections/Map.scala
@@ -172,3 +172,39 @@ trait AvlMapInstances {
     }
   }
 }
+
+class HashMap[K, V](val buckets: Vector[List[(K, V)]]) {
+
+  def +(pair: (K, V))(implicit K: Eq[K]): HashMap[K, V] = {
+    def updateBucket(bucket: List[(K, V)], key: K, value: V): List[(K, V)] =
+      bucket match {
+        case Nil => (key, value) :: Nil
+        case (bk, _) :: tail if bk === key => (key, value) :: tail
+        case head :: tail => head :: updateBucket(tail, key, value)
+      }
+
+    val (key, value) = pair
+    val keyBucketIndex = key.hashCode() % buckets.size
+    val keyBucket = buckets(keyBucketIndex)
+    val updatedKeyBucket = updateBucket(keyBucket, key, value)
+    new HashMap(buckets.updated(keyBucketIndex, updatedKeyBucket))
+  }
+
+  def get(key: K)(implicit K: Eq[K]): Option[V] = if(buckets.nonEmpty) {
+    val bucketIndex = key.hashCode % buckets.size
+    buckets(bucketIndex).collectFirst {
+      case (pkey, pvalue) if pkey === key => pvalue
+    }
+  } else None
+
+}
+
+object HashMap {
+
+  val defaultCapacity = 16
+
+  def apply[K, V](entries: (K, V)*)(implicit K: Eq[K]): HashMap[K, V] = entries.foldLeft(empty[K, V])(_ + _)
+
+  def empty[K, V]: HashMap[K, V] = new HashMap[K, V](Vector.fill(defaultCapacity)(List.empty))
+
+}
