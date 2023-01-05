4e32c3a482ebc97cfaabeb3460f8279dc7faabd6
satorg <satorg@gmail.com>
2023-01-04 20:43:46 -0800

    fix RangeSuite

--- tests/src/test/scala/cats/collections/RangeSuite.scala
+++ tests/src/test/scala/cats/collections/RangeSuite.scala
@@ -21,132 +21,367 @@
 
 package cats.collections
 
+import cats.Order
+import cats.Show
+import cats.syntax.order._
 import cats.syntax.show._
-import munit.FunSuite
+import munit.ScalaCheckSuite
+import org.scalacheck.Arbitrary
+import org.scalacheck.Cogen
+import org.scalacheck.Gen
+import org.scalacheck.Prop._
 
-class RangeSuite extends FunSuite {
-  import Range._
+class RangeSuite extends ScalaCheckSuite {
+  import RangeSuite._
 
-  test("contain items within [start, end]") {
-    val range = Range(1, 100)
+  property("contains(x): returns true for x within [start, end]") {
+    val gen = for {
+      range <- genAscRangeX
+      x <- genX(range)
+    } yield (range, x)
 
-    assert(scala.Range(1, 100).forall(i => range.contains(i)))
+    forAll(gen) { case (range: Range[X], x: X) =>
+      assert(range.contains(x))
+    }
+  }
+  property("contains(x): returns false for x < start") {
+    val gen = for {
+      range <- genAscRangeX(minX.succ, maxX)
+      x <- genX(minX, range.start.pred)
+    } yield (range, x)
+
+    forAll(gen) { case (range: Range[X], x: X) =>
+      assert(!range.contains(x))
+    }
+  }
+  property("contains(x): returns false for x > end") {
+    val gen = for {
+      range <- genAscRangeX(minX, maxX.pred)
+      x <- genX(range.end.succ, maxX)
+    } yield (range, x)
+
+    forAll(gen) { case (range: Range[X], x: X) =>
+      assert(!range.contains(x))
+    }
+  }
+  property("contains(Range): returns true if the passed range is within this range") {
+    val gen = for {
+      range1 <- genAscRangeX
+      range2 <- genAscRangeX(range1)
+    } yield (range1, range2)
+
+    forAll(gen) { case (range1: Range[X], range2: Range[X]) =>
+      assert(range1.contains(range2))
+    }
+  }
+  property("contains(Range): returns false if the passed range's start < this range's start") {
+    val gen = for {
+      range1 <- genAscRangeX(minX.succ, maxX)
+      range2 <- genAscRangeX(minX, range1.start.pred, maxX)
+    } yield (range1, range2)
+
+    forAll(gen) { case (range1: Range[X], range2: Range[X]) =>
+      assert(!range1.contains(range2))
+    }
+  }
+  property("contains(Range): returns false if the passed range's end > this range's end") {
+    val gen = for {
+      range1 <- genAscRangeX(minX, maxX.pred)
+      range2 <- genAscRangeX(minX, range1.end.succ, maxX)
+    } yield (range1, range2)
+
+    forAll(gen) { case (range1: Range[X], range2: Range[X]) =>
+      assert(!range1.contains(range2))
+    }
+  }
+  property("contains(Range): returns false if the passed range is a super-range of this one") {
+    val gen = for {
+      range1 <- genAscRangeX(minX.succ, maxX.pred)
+      range2 <- genAscRangeX(minX, range1.start.pred, range1.end.succ, maxX)
+    } yield (range1, range2)
+
+    forAll(gen) { case (range1: Range[X], range2: Range[X]) =>
+      assert(!range1.contains(range2))
+    }
   }
 
-  test("not contain items outside [start, end]") {
-    val range = Range(1, 100)
+  property("foreach: enumerates all values of a range") {
+    // Executes a passed `foreach` function and calculates some value that can be used for comparison.
+    def calc[A](f: A => Int, foreach: (A => Unit) => Unit) = {
+      val bldr = Vector.newBuilder[Int]
+      var count = 0
 
-    assertEquals(range.contains(0), false)
+      foreach { b =>
+        bldr += f(b)
+        count += 1
+      }
 
-    assertEquals(range.contains(101), false)
+      bldr.result()
+    }
+
+    forAll { (range: Range[X]) =>
+      val expected = calc[Int](identity, mkScalaRange(range).foreach)
+      val obtained = calc[X](_.x, range.foreach)
+      assertEquals(obtained, expected)
+    }
   }
 
-  test("contain sub range") {
-    val range = Range(1, 10)
+  property("map: returns a mapped range") {
+    forAll { (range: Range[String], f: String => X) =>
+      val expected = Range(f(range.start), f(range.end))
+      val obtained = range.map(f)
 
-    assert(range.contains(Range(2, 9)))
+      assertEquals(obtained, expected)
+    }
   }
 
-  test("apply function to each element") {
-    var s = 0
-
-    val range = Range(1, 100)
-
-    range.foreach(i => s += i)
-
-    assertEquals(scala.Range(1, 101).sum, s)
+  property("foldLeft: folds range values from start to end") {
+    forAll { (range: Range[X], s: String, f: (String, X) => String) =>
+      val expected = mkScalaRange(range).foldLeft(s) { (ss, x) => f(ss, X(x)) }
+      val obtained = range.foldLeft(s, f)
+      assertEquals(obtained, expected)
+    }
+  }
+  property("foldRight: folds range values from end to start") {
+    forAll { (range: Range[X], s: String, f: (X, String) => String) =>
+      val expected = mkScalaRange(range).foldRight(s) { (x, ss) => f(X(x), ss) }
+      val obtained = range.foldRight(s, f)
+      assertEquals(obtained, expected)
+    }
   }
 
-  test("map") {
-    val range = Range(1, 10)
+  property("diff(-): returns None if the passed range completely covers this one") {
+    val gen = for {
+      range1 <- genAscRangeX
+      range2 <- genAscRangeX(range1)
+    } yield (range1, range2)
 
-    val result = range.map(_ * 2).toList.sorted
+    forAll(gen) { case (range1: Range[X], range2: Range[X]) =>
+      assertEquals(range2 - range1, None)
+    }
+  }
+  property("diff(-): returns this range if it does not overlap the passed one") {
+    val gen = for {
+      range1 <- genAscRangeX(minX, maxX.pred)
+      range2 <- genAscRangeX(range1.end.succ, maxX)
+    } yield (range1, range2)
 
-    assertEquals(scala.Range(2, 21).toList.sorted, result)
+    forAll(gen) { case (range1: Range[X], range2: Range[X]) =>
+      assertEquals(range1 - range2, Some((range1, None)))
+      assertEquals(range2 - range1, Some((range2, None)))
+    }
+  }
+  property("diff(-): returns a single range if the passed range partially overlaps this one on the left") {
+    val gen = for {
+      range2 <- genAscRangeX(minX, maxX.pred)
+      range1 <- genAscRangeX(range2.start, range2.end, range2.end.succ, maxX)
+    } yield (range1, range2)
+
+    forAll(gen) { case (range1: Range[X], range2: Range[X]) =>
+      val expected = Range(range2.end.succ, range1.end)
+      val Some((obtained, None)) = range1 - range2
+      assertEquals(obtained, expected)
+    }
+  }
+  property("diff(-): returns a single range if the passed range partially overlaps this one on the right") {
+    val gen = for {
+      range2 <- genAscRangeX(minX.succ, maxX)
+      range1 <- genAscRangeX(minX, range2.start.pred, range2.start, range2.end)
+    } yield (range1, range2)
+
+    forAll(gen) { case (range1: Range[X], range2: Range[X]) =>
+      val expected = Range(range1.start, X(range2.start.x - 1))
+      val Some((obtained, None)) = range1 - range2
+      assertEquals(obtained, expected)
+    }
+  }
+  property("diff(-): returns two ranges if the passed range fully fits in the middle of this one") {
+    val gen = for {
+      range2 <- genAscRangeX(minX.succ, maxX.pred)
+      range1 <- genAscRangeX(minX, range2.start.pred, range2.end.succ, maxX)
+    } yield (range1, range2)
+
+    forAll(gen) { case (range1: Range[X], range2: Range[X]) =>
+      val expected1 = Range(range1.start, range2.start.pred)
+      val expected2 = Range(range2.end.succ, range1.end)
+
+      val Some((obtained1, Some(obtained2))) = range1 - range2
+      assertEquals(obtained1, expected1)
+      assertEquals(obtained2, expected2)
+    }
   }
 
-  test("foldLeft") {
-    val range = Range(1, 100)
+  property("add(+): returns a single range if one of the ranges is a subrange of the other") {
+    val gen = for {
+      range1 <- genAscRangeX
+      range2 <- genAscRangeX(range1)
+    } yield (range1, range2)
 
-    assertEquals(range.foldLeft[Int](0, (a, b) => a + b), scala.Range(1, 101).sum)
-    assertEquals(range.foldLeft[Int](0, (_, b) => b), 100)
+    forAll(gen) { case (range1: Range[X], range2: Range[X]) =>
+      val expected = Range(range1.start, range1.end)
+      val (obtained1, None) = range1 + range2
+      val (obtained2, None) = range2 + range1
+
+      assertEquals(obtained1, expected)
+      assertEquals(obtained2, expected)
+    }
+  }
+  property("add(+): returns a single range if two passed ranges are connected or patrially overlapping") {
+    val gen = for {
+      range1 <- genAscRangeX(minX, maxX.pred)
+      range2 <- genAscRangeX(range1.start, range1.end.succ, maxX)
+    } yield (range1, range2)
+
+    forAll(gen) { case (range1: Range[X], range2: Range[X]) =>
+      val expected = Range(range1.start, range2.end)
+      val (obtained1, None) = range1 + range2
+      val (obtained2, None) = range2 + range1
+
+      assertEquals(obtained1, expected)
+      assertEquals(obtained2, expected)
+    }
+  }
+  property("add(+): returns both passed ranges if there is a gap between them") {
+    val gen = for {
+      range1 <- genAscRangeX(minX, maxX.pred.pred) // two pred-s ensure the gap
+      range2 <- genAscRangeX(range1.end.succ.succ, maxX)
+    } yield (range1, range2)
+
+    forAll(gen) { case (range1: Range[X], range2: Range[X]) =>
+      val (obtained11, Some(obtained12)) = range1 + range2
+      val (obtained21, Some(obtained22)) = range2 + range1
+
+      assertEquals(obtained11, range1)
+      assertEquals(obtained12, range2)
+      assertEquals(obtained21, range1)
+      assertEquals(obtained22, range2)
+    }
   }
 
-  test("foldLeft in the right order") {
-    val range = Range(1, 100)
+  property("intersect(&): returns a smaller range if one of the ranges is subrange of the other") {
+    val gen = for {
+      range1 <- genAscRangeX
+      range2 <- genAscRangeX(range1)
+    } yield (range1, range2)
 
-    assertEquals(range.foldLeft[Int](0, (_, b) => b), 100)
+    forAll(gen) { case (range1: Range[X], range2: Range[X]) =>
+      val Some(obtained1) = range1 & range2
+      val Some(obtained2) = range2 & range1
+
+      assertEquals(obtained1, range2)
+      assertEquals(obtained2, range2)
+    }
+  }
+  property("intersect(&): returns None for non-overlapping ranged") {
+    val gen = for {
+      range1 <- genAscRangeX(minX, maxX.pred)
+      range2 <- genAscRangeX(range1.end.succ, maxX)
+    } yield (range1, range2)
+
+    forAll(gen) { case (range1: Range[X], range2: Range[X]) =>
+      assertEquals(range1 & range2, None)
+      assertEquals(range2 & range1, None)
+    }
   }
 
-  test("foldRight") {
-    val range = Range(1, 100)
-
-    assertEquals(range.foldRight[Int](0, (a, b) => a + b), scala.Range(1, 101).sum)
+  property("toInterator/toList: converts a range to Iterator/List") {
+    forAll { (range: Range[X]) =>
+      val expected = {
+        val step = if (range.start < range.end) 1 else -1
+        List.range(range.start.x, range.end.x, step).map(X(_)) :+ range.end
+      }
+      // Check both `toIterator` and `toList`.
+      assertEquals(range.toIterator.toList, expected)
+      assertEquals(range.toList, expected)
+    }
   }
 
-  test("foldRight in the right order") {
-    val range = Range(1, 100)
-
-    assertEquals(range.foldRight[Int](0, (a, _) => a), 1)
+  property("reverse: inverses a range") {
+    forAll { (range: Range[X]) =>
+      val expected = Range(range.end, range.start)
+      val obtained = range.reverse
+      assertEquals(obtained, expected)
+    }
   }
 
-  test("be able to diff (-)") {
-    val range = Range(1, 10)
+  property("show: renders a math representation of a range") {
+    forAll { (range: Range[X]) =>
+      val expected = s"[<${range.start.x}>, <${range.end.x}>]"
+      val obtained = range.show
+      assertEquals(obtained, expected)
+    }
+  }
+}
 
-    val Some((l, Some(r))) = range - Range(2, 9)
+object RangeSuite {
+  // TODO: this has to be placed in cats-collections-scalacheck
+  implicit def arbRange[XX](implicit arbX: Arbitrary[XX]): Arbitrary[Range[XX]] =
+    Arbitrary(
+      Arbitrary.arbitrary[(XX, XX)].map(Function.tupled(Range[XX]))
+    )
 
-    assertEquals(l.toList, List(1))
-    assertEquals(r.toList, List(10))
+  final private case class X(x: Int) {
+    import X._
+    // Fails tests early if x exceed the allowed range.
+    require(
+      x >= minVal && x <= maxVal,
+      s"x must be in range [$minVal, $maxVal] but got $x"
+    )
 
-    val x1 = range - range
+    def succ: X = X(x + 1)
+    def pred: X = X(x - 1)
+  }
+  private object X {
+    final val minVal = 0
+    final val maxVal = 99
+  }
+  private val minX = X(X.minVal)
+  private val maxX = X(X.maxVal)
 
-    assertEquals(x1.isDefined, false)
+  private def genX(min: X, max: X): Gen[X] = Gen.chooseNum(min.x, max.x).map(X(_))
+  private def genX(range: Range[X]): Gen[X] = genX(range.start, range.end)
+  private val genX: Gen[X] = genX(minX, maxX)
 
-    val Some((x2, None)) = range - Range(-1, 5)
+  implicit private val scalaOrdX: Ordering[X] = Ordering.by(_.x)
+  implicit private val catsOrdX: Order[X] = Order.fromOrdering[X]
+  implicit private val catsShowX: Show[X] = Show.show(x => s"<${x.x}>")
 
-    assertEquals(x2.toList, List(6, 7, 8, 9, 10))
+  implicit private val schkArbX: Arbitrary[X] = Arbitrary(genX)
+  implicit private val schkCogX: Cogen[X] = Cogen[Int].contramap(_.x)
 
-    val Some((x3, None)) = range - Range(3, 12)
-
-    assertEquals(x3.toList, List(1, 2))
+  implicit private val catsDiscrX: Discrete[X] = new Discrete[X] {
+    override def succ(x: X): X = x.succ
+    override def pred(x: X): X = x.pred
   }
 
-  test("return an iterator for the range") {
-    assertEquals(Range(0, 10).toIterator.toList, List.range(0, 11)) // [0, 10]
+  private def genAscRangeX(
+    minStart: X,
+    maxStart: X,
+    minEnd: X,
+    maxEnd: X
+  ): Gen[Range[X]] = {
+    require(minStart >= minX && minStart <= maxX, s"incorrect minStart=$minStart")
+    require(maxStart >= minStart && maxStart <= maxX, s"incorrect maxStart=$maxStart (minStart=$minStart)")
+    require(minEnd >= minStart && minEnd <= maxX, s"incorrect minEnd=$minEnd (minStart=$minStart)")
+    require(maxEnd >= minEnd && maxEnd <= maxX, s"incorrect maxEnd (minEnd=$minEnd)")
+    for {
+      start <- genX(minStart, maxStart)
+      end <- genX(Order.max(minEnd, start), maxEnd)
+    } yield {
+      assert(start <= end, s"$start is not less or equal to $end")
+      Range(start, end)
+    }
   }
+  private def genAscRangeX(min: X, mid: X, max: X): Gen[Range[X]] =
+    genAscRangeX(min, mid, mid, max)
+  private def genAscRangeX(min: X, max: X): Gen[Range[X]] =
+    genAscRangeX(min, max, min, max)
 
-  test("return an iterator for a reversed range") {
-    assertEquals(Range(10, 0).toIterator.toList, List.range(10, -1, -1)) // [10, 0]
-  }
+  private def genAscRangeX(range: Range[X]): Gen[Range[X]] =
+    genAscRangeX(range.start, range.end)
 
-  test("return an iterator when the range contains a single value") {
-    assertEquals(Range(3, 3).toIterator.toList, List(3))
-  }
+  private val genAscRangeX: Gen[Range[X]] =
+    genAscRangeX(minX, maxX)
 
-  test("generate inverted range") {
-    val range = Range(5, 1)
-
-    assertEquals(range.toList, List(5, 4, 3, 2, 1))
-  }
-
-  test("map inverted range") {
-    val range = Range(5, 1)
-
-    val result = range.map(_ * 2).toList
-
-    assertEquals(result, List(10, 9, 8, 7, 6, 5, 4, 3, 2))
-  }
-
-  test("the reverse be equals to the reverted range") {
-    val range = Range(20, 50)
-    val other = Range(50, 20)
-
-    assertEquals(range.reverse.toList, other.toList)
-  }
-
-  test("be convertible to string in math form") {
-    val range = Range(10, 20)
-
-    assertEquals(range.show, "[10, 20]")
-  }
+  private def mkScalaRange(range: Range[X]) = range.start.x to range.end.x
 }
